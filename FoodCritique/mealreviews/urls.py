7932711830983440d6b07180e59be9ea9e130ba7
SinistralMars <paulk2@student.byu.edu>
2022-11-02 16:42:25 -0600

    More Views

--- FoodCritique/mealreviews/urls.py
+++ FoodCritique/mealreviews/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, searchPageView, restuarantPageView, ratingPageView
 
 urlpatterns = [
     path('', indexPageView, name = 'index'),
+    path('Search', searchPageView, name = 'Search'),
+    path('Restuarant', restuarantPageView, name = 'Restuarant'),
+    path('Rating', ratingPageView, name = 'Rating'),
 ]
--- FoodCritique/mealreviews/views.py
+++ FoodCritique/mealreviews/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    return HttpResponse("Reviews")
+    return HttpResponse("index")
+def searchPageView(request) :
+    return HttpResponse("Search")
+def restuarantPageView(request) :
+    return HttpResponse("Restuarant")
+def ratingPageView(request) :
+    return HttpResponse("Rating")

c3227c743225fbf64733fd172a6416db7dcd3abe
SinistralMars <paulk2@student.byu.edu>
2022-11-16 16:25:15 -0700

    New

--- FoodCritique/mealreviews/models.py
+++ FoodCritique/mealreviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model) :
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    score = models.DecimalField(max_digits=1, decimal_places=1)
+    cuisineType = models.CharField(max_length=30)
+
+    def __str__(self) :
+        return (self.name)
+
+    class Meta :
+        db_table = 'Restaurants'
+
+class Dish(models.Model) :
+    restuarant = models.ForeignKey(Restaurant, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=30)
+    score =  models.DecimalField(max_digits=1, decimal_places=1)
+    description = models.CharField(max_length=100)
+    
+    def __str__(self) :
+        return (self.name)
+    class Meta :
+        db_table = 'Dish'
--- FoodCritique/mealreviews/urls.py
+++ FoodCritique/mealreviews/urls.py
@@ -4,9 +4,9 @@ from .views import indexPageView, searchPageView, restuarantPageView, ratingPage
 urlpatterns = [
     path('', indexPageView, name = 'index'),
     
-    path('Create', createPageView, name = 'Create'), #Create
-    path('Search', searchPageView, name = 'Search'), #Read
-    path('Rating', ratingPageView, name = 'Rating'), #Read
-    path('Restuarant', restuarantPageView, name = 'Restuarant'), #Update
-    path('Delete', deletePageView, name = 'Delete'), #Delete
+    path('create', createPageView, name = 'create'), #Create
+    path('search', searchPageView, name = 'search'), #Read
+    path('rating', ratingPageView, name = 'rating'), #Read
+    path('restuarant', restuarantPageView, name = 'restuarant'), #Update
+    path('delete', deletePageView, name = 'delete'), #Delete
 ]

e3421dced544746b8fd46455f15ac9127c06946a
SinistralMars <paulk2@student.byu.edu>
2022-12-12 16:27:42 -0700

    Bootstrap

--- FoodCritique/mealreviews/urls.py
+++ FoodCritique/mealreviews/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from .views import indexPageView, searchPageView, restaurantPageView, ratingPageView, createPageView, deletePageView
 
 urlpatterns = [
-    path('', indexPageView, name = 'index'),
-    
     path('create', createPageView, name = 'create'), #Create
     path('search', searchPageView, name = 'search'), #Read
     path('rating', ratingPageView, name = 'rating'), #Read
     path('restaurant', restaurantPageView, name = 'restaurant'), #Update
     path('delete', deletePageView, name = 'delete'), #Delete
+    path('', indexPageView, name = 'index'),
 ]

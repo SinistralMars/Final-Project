0d77ed6828a35792cc73719a62f87eb4e3eed998
mikeygoltra <106622395+mikeygoltra@users.noreply.github.com>
2022-12-13 12:45:25 -0700

    deployment

--- FoodCritique/foodcritique/settings.py
+++ FoodCritique/foodcritique/settings.py
@@ -80,14 +80,17 @@ WSGI_APPLICATION = 'foodcritique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'foodCritique',
-        'USER': 'postgres',
-        'PASSWORD': 'Busterg123',
-        'HOST': 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'foodCritique',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Busterg123',
+    #     'HOST': 'localhost'
+    # }
 }
 
 
